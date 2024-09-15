8446bb571903e36e8faa52bec3f31cda55ec6bfe
Samatov Marufjon <m.samatov@orienbank.tj>
2024-09-15 16:43:17 +0500

    The main app is ready, pages index and about

--- main/views.py
+++ main/views.py
@@ -4,11 +4,16 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Главная страница магазина - Home'
+        "title": "Home - Главная",
+        "content": "Магазин мебели HOME",
     }
-    return render(request, 'main/index.html', context)
+    return render(request, "main/index.html", context)
 
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        "title": "Home - О Нас",
+        "content": "О Нас",
+        "text_on_page": "Наш магазин предлагает стильную и качественную мебель для дома и офиса. Широкий выбор, современные решения, комфорт и долговечность — всё для создания идеального интерьера.",
+    }
+    return render(request, "main/about.html", context)
