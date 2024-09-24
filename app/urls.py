3b0f3406b66e939aba6cfe989d5c713021400b0f
Samatov Marufjon <m.samatov@orienbank.tj>
2024-09-24 06:27:11 +0500

    Registered simple tag for categories and settings mediafiles

--- app/urls.py
+++ app/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
+from app.settings import DEBUG
 from django.contrib import admin
 from django.urls import path, include
 from debug_toolbar.toolbar import debug_toolbar_urls
@@ -23,4 +26,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls", namespace="main")),
     path("catalog/", include("goods.urls", namespace="catalog")),
-] + debug_toolbar_urls()
+]  
+
+if settings.DEBUG:
+    urlpatterns +=  debug_toolbar_urls()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- goods/views.py
+++ goods/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 
+from goods.models import Products
+
 
 def catalog(request):
+
+    goods = Products.objects.all()
     context = {
         "title": "Home - Каталог",
-        
+        "goods": goods,
     }
     return render(request, "goods/catalog.html", context)
 
