06a3b7827c655e8b309350afac9312c713b57268
Samatov Marufjon <m.samatov@orienbank.tj>
2024-11-17 23:27:43 +0500

    Full text search well-done

--- app/settings.py
+++ app/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
+    "django.contrib.postgres",
     "debug_toolbar",
 
     "main.apps.MainConfig",
@@ -84,8 +84,12 @@ WSGI_APPLICATION = "app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'lobby',
+        'USER': 'lobby',
+        'PASSWORD': 'Orien001$1',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
