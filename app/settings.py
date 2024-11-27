cce74410cd330449dcfcab7b1ffbe15690a6c909
Samatov Marufjon <m.samatov@orienbank.tj>
2024-11-28 02:26:22 +0500

    create userapp

--- app/settings.py
+++ app/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import MEDIA_ROOT, MEDIA_URL
+from django.conf.global_settings import AUTH_USER_MODEL, MEDIA_ROOT, MEDIA_URL
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.postgres",
     "debug_toolbar",
-
+    "users.apps.UsersConfig",
     "main.apps.MainConfig",
     "goods.apps.GoodsConfig",
 ]
@@ -146,3 +146,4 @@ INTERNAL_IPS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = 'users.USER'
