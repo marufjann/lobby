402043b306c664034e7bf352beb1147dfe0f05b0
Samatov Marufjon <m.samatov@orienbank.tj>
2024-12-04 05:24:40 +0500

    Registration form and controller are ready

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,7 @@
+from dataclasses import field
+from pyexpat import model
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from users.models import User
 
 class UserLoginForm(AuthenticationForm):
@@ -22,3 +24,20 @@ class UserLoginForm(AuthenticationForm):
     #                                     'class': 'form-control',
     #                                     'placeholder': 'Введите ваш пароль'})
     # )
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+            )
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
